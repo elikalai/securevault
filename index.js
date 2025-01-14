d02a48eac4d0ba5f0da57649a738eea79d6512eb
Elijah Irvine <elikalaixiv@gmail.com>
2025-01-14 09:53:44 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const code1 = 7 + 3;
+console.log(code1)
+
+const code2 = 20 + 21 - 1;
+console.log(code2)
+
+const code3 = 15 % 3 + 39;
+console.log(code3)
+
+const message = "The vault has been secured. The combination is:"
+console.log(message)
+
+const codeA = code1 + " " + code2 + " " + code3;
+console.log(codeA)
+
+const codeB = code1 + " " + code2 + " " + code3;
+console.log(codeB)
+
+console.log(message + codeA + codeB)
